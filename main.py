c9a8a4e432d1908039bafdbb219f211ba3afa4ab
Ignacio <ignacio.quintanilla@hotmail.com>
2025-04-10 20:39:05 +0200

    Main Encoder Components

---
+++ main.py
@@ -0,0 +1,144 @@
+import torch
+import math
+import torch.nn as nn
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None: 
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len, d_model), 1 d_model vector per token in sequence
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len). Unsqeeze inserts new dim at arg pos, so (seq_len, 1)
+        position = torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1) 
+
+        den_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply the sine to even positions
+        pe[:,::2] = torch.sin(position*den_term)
+        # Apply the cosine to odd positions
+        pe[:,1::2] = torch.cos(position*den_term)
+
+        # Add the batch dimension so tensor of size (1, seq_len, d_model)
+        pe = pe.unsqueeze(0) 
+
+        # Save tensor as part of model although it is not learned parameter
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # Requires grad makes tensor not learned
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
+        return self.dropout(x)
+    
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        # Epsilon prevents very large numbers or very small numbers (numerical stability).
+        # Also prevents division by 0
+        self.eps = eps
+        # Multiplied learned parameter
+        self.alpha = nn.Parameter(torch.ones(1))
+        # Added learned paramter
+        self.bias = nn.Parameter(torch.ones(1))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x-mean) / (std*self.eps) + self.bias
+    
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        # W1 and B1
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        # W2 and B1
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, 'd_model is not divisible by h!'
+        self.dropout = nn.Dropout(dropout)
+        self.d_k = d_model // h 
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) --> (Batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        # (Batch, h, seq_len, seq_len)
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask): # Mask prevents interaction between prescribed tokens
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model) for all three
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        # Take a subset of the embedding dims and pass them to different heads
+        # h dim to be 2nd dim, so each head sees all sentences
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_k) --> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2) 
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2) 
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, seq_len, d_k) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h*self.d_k)
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+    
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
